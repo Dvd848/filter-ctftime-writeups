cb776540429dad663615a8caeb870a5df9c05c5b
Dvd848 <Dvd848@users.noreply.github.com>
2020-11-22 22:59:33 +0200

    Run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+export FLASK_RUN_PORT=5000
+export FLASK_RUN_HOST=0.0.0.0
+export FLASK_ENV=development
+
+echo "Interfaces:"
+ifconfig | grep "inet " | awk '{printf " (-) %s\n", $2}'
+
+flask run
